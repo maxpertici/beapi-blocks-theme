f0bd510eee0a0eb4e0a259da47020f657a444f90
Nicolas Langle <nlangle@beapi.fr>
2025-03-27 17:42:41 +0100

    feat (theme): home, work in progress

--- patterns/hidden-card-post-2.php
+++ patterns/hidden-card-post-2.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: Hidden card post 1
- * Slug: beapi-blocks-theme/hidden-card-post-1
+ * Title: Hidden card post 2
+ * Slug: beapi-blocks-theme/hidden-card-post-2
  * Description: Card post.
  * Inserter: no
  *
@@ -11,13 +11,13 @@
  */
 
 ?>
-<!-- wp:group {"tagName":"article","className":"wp-pattern-hidden-card wp-pattern-hidden-card-post wp-pattern-hidden-card-post-1","layout":{"type":"constrained"},"metadata":{"patternName":"beapi-blocks-theme/hidden-card-post-1","name":"Hidden card post 1"}} -->
-<article class="wp-block-group wp-pattern-hidden-card wp-pattern-hidden-card-post wp-pattern-hidden-card-post-1">
+<!-- wp:group {"tagName":"article","className":"wp-pattern-hidden-card wp-pattern-hidden-card-post wp-pattern-hidden-card-post-2","layout":{"type":"constrained"},"metadata":{"patternName":"beapi-blocks-theme/hidden-card-post-1","name":"Hidden card post 1"}} -->
+<article class="wp-block-group wp-pattern-hidden-card wp-pattern-hidden-card-post wp-pattern-hidden-card-post-2">
 	<!-- wp:post-featured-image {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|s"}}}} /-->
 	<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}}}} -->
 	<div class="wp-block-group" style="margin-top:0">
 		<!-- wp:post-date {"className":"is-style-label","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|s"}}}} /-->
-		<!-- wp:post-title {"isLink":true,"level":3,"className":"is-style-h4","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|s"}}}} /-->
+		<!-- wp:post-title {"isLink":true,"level":2,"className":"is-style-h3","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|s"}}}} /-->
 		<!-- wp:post-terms {"term":"category","className":"is-style-tag"} /-->
 	</div>
 	<!-- /wp:group -->
---
+++ patterns/hidden-queried-object-title.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Hidden queried object title
+ * Slug: beapi-blocks-theme/hidden-queried-object-title
+ * Description: Hidden heading for the archive page.
+ * Inserter: no
+ *
+ * @package WordPress
+ * @subpackage BeAPI Blocks Theme
+ * @since BeAPI Blocks Theme 1.0
+ */
+
+?>
+<!-- wp:heading {"level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide"><?php echo esc_html( get_the_title( get_queried_object_id() ) ); ?></h1>
+<!-- /wp:heading -->
