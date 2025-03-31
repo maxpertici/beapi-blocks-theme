fca284c6d2fd75cb074f449daa8b80c8a06445af
Nicolas Langle <nlangle@beapi.fr>
2025-03-31 15:42:41 +0200

    fix (theme): home grid 1

--- patterns/hidden-home-content-2.php
+++ patterns/hidden-home-content-2.php
@@ -10,20 +10,22 @@
  */
 
 ?>
-<!-- wp:query {"query":{"perPage":1,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null,"parents":[],"format":[]},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|2-xl"}}}} -->
-<div class="wp-block-query alignwide" style="margin-bottom:var(--wp--preset--spacing--2-xl)">
-	<!-- wp:post-template -->
-		<!-- wp:pattern {"slug":"beapi-blocks-theme/hidden-card-post-4"} /-->
-	<!-- /wp:post-template -->
-</div>
-<!-- /wp:query -->
-
 <!-- wp:pattern {"slug":"beapi-blocks-theme/hidden-filters-home"} /-->
 
-<!-- wp:query {"query":{"perPage":12,"pages":0,"offset":"1","postType":"post","order":"asc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"format":[]},"align":"wide","layout":{"type":"default"},"metadata":{"wpgb":"wpgb-content-block/5a6df3f810f245aabbcc4c60b511647f"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|xl"}}}} -->
+<!-- wp:query {"query":{"perPage":12,"pages":0,"offset":"0","postType":"post","order":"asc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"format":[]},"align":"wide","layout":{"type":"default"},"metadata":{"wpgb":"wpgb-content-block/5a6df3f810f245aabbcc4c60b511647f"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|xl"}}}} -->
 <div class="wp-block-query alignwide" style="margin-bottom:var(--wp--preset--spacing--xl)">
 	<!-- wp:post-template {"className":"grid-home-2","align":"full","layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"18rem"}} -->
-		<!-- wp:pattern {"slug":"beapi-blocks-theme/hidden-card-post-3"} /-->
+		<?php
+		get_template_part(
+			'patterns/hidden-card-post-1',
+			null,
+			[
+				'card_additional_classes' => 'wp-pattern-hidden-card-post-3',
+				'heading_level'           => 2,
+				'heading_class'           => 'is-style-h5',
+			]
+		);
+		?>
 	<!-- /wp:post-template -->
 </div>
 <!-- /wp:query -->
