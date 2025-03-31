46a426cb5893ca9741b85653344f925ba3b4fd62
Nicolas Langle <nlangle@beapi.fr>
2025-03-31 09:32:04 +0200

    feat (theme): split home into multiple patterns

---
+++ patterns/hidden-home-filters.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Home filters
+ * Slug: beapi-blocks-theme/hidden-home-filters
+ * Inserter: no
+ *
+ * @package WordPress
+ * @subpackage BeAPI Blocks Theme
+ * @since BeAPI Blocks Theme 1.0
+ */
+
+?>
+<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|2-xl"}}}} -->
+<div class="wp-block-group alignwide" style="margin-bottom:var(--wp--preset--spacing--2-xl)">
+	<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/5a6df3f810f245aabbcc4c60b511647f","style":0,"id":1} /-->
+	<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/5a6df3f810f245aabbcc4c60b511647f","style":0,"id":2} /-->
+	<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/5a6df3f810f245aabbcc4c60b511647f","style":0,"id":3} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|xl"}}}} -->
+<div class="wp-block-group alignwide" style="margin-bottom:var(--wp--preset--spacing--xl)">
+	<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/5a6df3f810f245aabbcc4c60b511647f","id":6} /-->
+	<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/5a6df3f810f245aabbcc4c60b511647f","id":5} /-->
+</div>
+<!-- /wp:group -->
