96176dc906cf3ee9adeb24e8645bbc66e82d8007
mricoul <mricoul@beapi.fr>
2025-05-16 09:04:22 +0200

    feat (single-job-offer): add template

---
+++ patterns/job-offer-application.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Job offer application
+ * Slug: beapi-blocks-theme/job-offer-application
+ * Description: Displays a job offer application button.
+ *
+ * @package WordPress
+ * @subpackage BeAPI Blocks Theme
+ * @since BeAPI Blocks Theme 1.0
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Job offer application"},"align":"full","style":{"spacing":{"margin":{"top":"var:preset|spacing|2-xl"},"padding":{"top":"var:preset|spacing|2-xl","bottom":"var:preset|spacing|2-xl"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--2-xl);padding-top:var(--wp--preset--spacing--2-xl);padding-bottom:var(--wp--preset--spacing--2-xl)"><!-- wp:paragraph {"align":"center","className":"is-style-h3"} -->
+<p class="has-text-align-center is-style-h3">Are you interested in this job offer?</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">We look forward to receiving your application.<br>All applications will be treated confidentially.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#">Send my application</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
