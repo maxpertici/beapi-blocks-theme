20946d0fd2b5b1b08c8a5e52f1c441c30958d590
mricoul <mricoul@beapi.fr>
2025-05-16 11:45:00 +0200

    fix (patterns): code review for more-job-offers

--- patterns/more-job-offers.php
+++ patterns/more-job-offers.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Related job offers
- * Slug: beapi-blocks-theme/related-job-offers
+ * Slug: beapi-blocks-theme/more-job-offers
  *
  * @package WordPress
  * @subpackage BeAPI Blocks Theme
@@ -18,7 +18,7 @@
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
 <div class="wp-block-buttons"><!-- wp:button {"className":"is-style-link"} -->
-<div class="wp-block-button is-style-link"><a class="wp-block-button__link wp-element-button" href="#"><?php esc_html_e( 'See all', 'beapi-blocks-theme' ); ?></a></div>
+<div class="wp-block-button is-style-link"><a class="wp-block-button__link wp-element-button" href="<?php echo esc_url( get_post_type_archive_link( 'job-offer' ) ); ?>"><?php esc_html_e( 'See all', 'beapi-blocks-theme' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:column -->
@@ -26,22 +26,12 @@
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:query {"queryId":29,"query":{"perPage":4,"pages":0,"offset":0,"postType":"job-offer","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"format":[]},"metadata":{"categories":["posts"],"patternName":"beapi-blocks-theme/template-query-job-offers","name":"List of job offers"}} -->
 <div class="wp-block-query"><!-- wp:post-template {"align":"full","className":"grid-job-offers-1","style":{"spacing":{"blockGap":"var:preset|spacing|md"}},"layout":{"type":"grid","columnCount":1,"minimumColumnWidth":null}} -->
-<!-- wp:group {"tagName":"article","metadata":{"patternName":"beapi-blocks-theme/hidden-card-job-offer-1","name":"Hidden card job offer"},"className":"wp-pattern-hidden-card wp-pattern-hidden-card-job-offer","style":{"border":{"bottom":{"color":"var:preset|color|gray-100","width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|md"},"blockGap":"var:preset|spacing|2-xs"}},"layout":{"type":"constrained"}} -->
-<article class="wp-block-group wp-pattern-hidden-card wp-pattern-hidden-card-job-offer" style="border-bottom-color:var(--wp--preset--color--gray-100);border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--md)"><!-- wp:post-title {"isLink":true,"align":"full","className":"is-style-h4"} /-->
-
-<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|md"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group alignfull"><!-- wp:post-terms {"term":"contract-type","className":"is-style-label"} /-->
-
-<!-- wp:post-terms {"term":"seniority-level","className":"is-style-label"} /-->
-
-<!-- wp:post-terms {"term":"location","className":"is-style-label"} /--></div>
-<!-- /wp:group --></article>
-<!-- /wp:group -->
+<!-- wp:pattern {"slug":"beapi-blocks-theme/hidden-card-job-offer-1"} /-->
 <!-- /wp:post-template -->
 
 <!-- wp:query-no-results -->
-<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-<p></p>
+<!-- wp:paragraph -->
+<p><?php echo esc_html_x( 'Sorry, but nothing was found. Please try a search with different keywords.', 'Message explaining that there are no results returned from a search.', 'beapi-blocks-theme' ); ?></p>
 <!-- /wp:paragraph -->
 <!-- /wp:query-no-results --></div>
 <!-- /wp:query --></div>
