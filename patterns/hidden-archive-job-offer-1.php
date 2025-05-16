abf30323bae898b15d8a14460c6e317045a2ac80
mricoul <mricoul@beapi.fr>
2025-05-16 10:34:23 +0200

    fix (archive-job-offers): code review

--- patterns/hidden-archive-job-offer-1.php
+++ patterns/hidden-archive-job-offer-1.php
@@ -26,13 +26,13 @@
 <!-- wp:columns {"align":"wide","className":"wp-pattern-archive-columns","style":{"spacing":{"margin":{"top":"var:preset|spacing|2-xl"},"blockGap":{"left":"var:preset|spacing|4-xl"}}}} -->
 <div class="wp-block-columns alignwide wp-pattern-archive-columns" style="margin-top:var(--wp--preset--spacing--2-xl)"><!-- wp:column {"width":"27%","style":{"spacing":{"padding":{"top":"var:preset|spacing|l"}}}} -->
 <div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--l);flex-basis:27%"><!-- wp:group {"className":"wp-pattern-hidden-filters","layout":{"type":"constrained"}} -->
-<div class="wp-block-group wp-pattern-hidden-filters"><!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/3e43cf251adf4a8a8440918a5486f6c7","id":8} /-->
+<div class="wp-block-group wp-pattern-hidden-filters"><!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/8aea04cc87d94d57ad885cb8533b55b8","id":13} /-->
 
-<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/3e43cf251adf4a8a8440918a5486f6c7","id":10} /-->
+<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/8aea04cc87d94d57ad885cb8533b55b8","id":14} /-->
 
-<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/3e43cf251adf4a8a8440918a5486f6c7","id":1} /-->
+<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/8aea04cc87d94d57ad885cb8533b55b8","id":15} /-->
 
-<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/3e43cf251adf4a8a8440918a5486f6c7","id":9} /--></div>
+<!-- wp:wp-grid-builder/facet {"grid":"wpgb-content-block/8aea04cc87d94d57ad885cb8533b55b8","id":16} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
@@ -46,12 +46,12 @@
 
 <!-- wp:query-no-results -->
 <!-- wp:paragraph -->
-<p>Sorry, but nothing was found. Please try a search with different keywords.</p>
+<p><?php echo esc_html_x( 'Sorry, but nothing was found. Please try a search with different keywords.', 'Message explaining that there are no results returned from a search.', 'beapi-blocks-theme' ); ?></p>
 <!-- /wp:paragraph -->
 <!-- /wp:query-no-results --></div>
 <!-- /wp:query -->
 
-<!-- wp:wp-grid-builder/facet {"align":"center","grid":"wpgb-content-block/8aea04cc87d94d57ad885cb8533b55b8","id":5} /-->
+<!-- wp:wp-grid-builder/facet {"align":"center","grid":"wpgb-content-block/8aea04cc87d94d57ad885cb8533b55b8","id":4} /-->
 <!-- wp:pattern {"slug":"beapi-blocks-theme/unsolicited-application"} /-->
 </div>
 <!-- /wp:column --></div>
--- patterns/hidden-card-job-offer-1.php
+++ patterns/hidden-card-job-offer-1.php
@@ -9,16 +9,42 @@
  * @subpackage BeAPI Blocks Theme
  * @since BeAPI Blocks Theme 1.0
  */
+
+$icons_uri = get_template_directory_uri() . '/dist/icons/';
 ?>
 
 <!-- wp:group {"tagName":"article","metadata":{"patternName":"beapi-blocks-theme/hidden-card-job-offer-1","name":"Hidden card job offer"},"className":"wp-pattern-hidden-card wp-pattern-hidden-card-job-offer","style":{"border":{"bottom":{"color":"var:preset|color|gray-100","width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|md"},"blockGap":"var:preset|spacing|2-xs"}},"layout":{"type":"constrained"}} -->
 <article class="wp-block-group wp-pattern-hidden-card wp-pattern-hidden-card-job-offer" style="border-bottom-color:var(--wp--preset--color--gray-100);border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--md)"><!-- wp:post-title {"isLink":true,"align":"full","className":"is-style-h4"} /-->
 
 <!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|md"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group alignfull"><!-- wp:post-terms {"term":"contract-type","className":"is-style-label"} /-->
+<div class="wp-block-group alignfull"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|3-xs"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:beapi/icon-block -->
+<ul class="wp-block-beapi-icon-block"><!-- wp:beapi/icon-item {"icon":{"label":"File","name":"icon-file","type":"sprite"},"size":20} -->
+<li class="wp-block-beapi-icon-item"><span><svg width="20" height="20" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" class="wp-block-beapi-icon-block__icon-component wp-block-beapi-icon-block__icon-component--sprite icon-file" aria-hidden="true"><use href="<?php echo esc_url( $icons_uri ); ?>/sprite.svg#icon-file"></use></svg></span></li>
+<!-- /wp:beapi/icon-item --></ul>
+<!-- /wp:beapi/icon-block -->
 
-<!-- wp:post-terms {"term":"seniority-level","className":"is-style-label"} /-->
+<!-- wp:post-terms {"term":"contract-type","className":"is-style-label"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|3-xs"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:beapi/icon-block -->
+<ul class="wp-block-beapi-icon-block"><!-- wp:beapi/icon-item {"icon":{"label":"User","name":"icon-user","type":"sprite"},"size":20} -->
+<li class="wp-block-beapi-icon-item"><span><svg width="20" height="20" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" class="wp-block-beapi-icon-block__icon-component wp-block-beapi-icon-block__icon-component--sprite icon-user" aria-hidden="true"><use href="<?php echo esc_url( $icons_uri ); ?>/sprite.svg#icon-user"></use></svg></span></li>
+<!-- /wp:beapi/icon-item --></ul>
+<!-- /wp:beapi/icon-block -->
+
+<!-- wp:post-terms {"term":"seniority-level","className":"is-style-label"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|3-xs"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:beapi/icon-block -->
+<ul class="wp-block-beapi-icon-block"><!-- wp:beapi/icon-item {"icon":{"label":"Map marker","name":"icon-map-marker","type":"sprite"},"size":20} -->
+<li class="wp-block-beapi-icon-item"><span><svg width="20" height="20" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" class="wp-block-beapi-icon-block__icon-component wp-block-beapi-icon-block__icon-component--sprite icon-map-marker" aria-hidden="true"><use href="<?php echo esc_url( $icons_uri ); ?>/sprite.svg#icon-map-marker"></use></svg></span></li>
+<!-- /wp:beapi/icon-item --></ul>
+<!-- /wp:beapi/icon-block -->
 
 <!-- wp:post-terms {"term":"location","className":"is-style-label"} /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></article>
 <!-- /wp:group -->
